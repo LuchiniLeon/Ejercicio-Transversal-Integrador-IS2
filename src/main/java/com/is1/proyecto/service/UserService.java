9e4e177389f716e0c4563d124ec81209d744721d
Luchini-Inspiron <luchini.un612@gmail.com>
2026-05-03 18:39:55 -0300

    User service modificado para usar campos nuevos de user, ahora crea persona y luego crea usuario

--- src/main/java/com/is1/proyecto/service/UserService.java
+++ src/main/java/com/is1/proyecto/service/UserService.java
@@ -2,26 +2,42 @@ package com.is1.proyecto.service;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import com.is1.proyecto.models.Persona;
 import com.is1.proyecto.models.User;
 
 public class UserService {
-    public static void createUser(String name, String password) {
+    public static void createUser(String name, String password, String nombre, String apellido, String fechaNacimiento, Integer dni) {
 
         // VALIDACIONES
         if (name == null || name.isEmpty() ||
-            password == null || password.isEmpty()) {
+            password == null || password.isEmpty() ||
+            nombre == null || nombre.isEmpty() ||
+            apellido == null || apellido.isEmpty() ||
+            fechaNacimiento == null || fechaNacimiento.isEmpty() ||
+            dni == null || dni == 0 ) {
 
-            throw new IllegalArgumentException("Nombre y contraseña son requeridos");
+            throw new IllegalArgumentException("Todos los campos son requeridos");
         }
 
-        // CREACIÓN
+        //CREACION DE PERSONA
+        Persona persona = new Persona();
+        persona.setNombre(nombre);
+        persona.setApellido(apellido);
+        persona.setFechaNac(fechaNacimiento);
+        persona.setDNI(dni);
+
+        persona.saveIt();
+
+        // CREACIÓN DE USUARIO
         User user = new User();
 
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
-        user.set("name", name);
-        user.set("password", hashedPassword);
-        user.set("esAdministrador", 0);
+        user.setName(name);
+        user.setPassword(hashedPassword);
+
+        //dni del usuario es el que ya fue agregado a la tabla persona (Debe ser creada la presona primero para poder asignarlo)
+        user.set("dni_Persona", persona.getDNI());
 
         user.saveIt();
     }   
