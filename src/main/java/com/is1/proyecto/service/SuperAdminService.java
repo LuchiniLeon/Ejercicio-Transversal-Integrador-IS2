012bdeee56fb2e59d0f72c7e78a9aea0559ee9a0
Luchini-Inspiron <luchini.un612@gmail.com>
2026-06-06 12:39:02 -0300

    Correcciones a la inserción de los usuarios harcodeados

--- src/main/java/com/is1/proyecto/service/SuperAdminService.java
+++ src/main/java/com/is1/proyecto/service/SuperAdminService.java
@@ -26,24 +26,21 @@ public class SuperAdminService {
 
             UserService.createUser(admin.getUsuario(), admin.getPassword(), admin.getNombre(), admin.getApellido(), admin.getFecha(), admin.getDni(), admin.getEmail());
 
-            String sql = "INSERT INTO administrador (dni_Persona, cargo, sector) VALUES (?, ?, ?)";
-
-            Base.exec(sql, admin.getDni(), admin.getCargo(), admin.getSector());
-            
+            AdminService.crearAdmin(admin.getDni(), admin.getCargo(), admin.getSector());
 
             // Hardcodeo — solo si no existen
-            if (User.findFirst("nombreUsuario = ?", "Agos100") == null)
-                UserService.createUser("Agos100", "Agos100", "Agostina", "Dios Sabe", "1900-05-23", 48458455, "mechylacruz06@gmail.com");
+            if (User.findFirst("nombreUsuario = ?", "Agos100") == null){
+                UserService.createUser("Agos100", "Agos100", "Agostina", "Dios Sabe", "1900-05-23", 48458455, "agos100@gmail.com");
                 Base.exec("INSERT INTO administrador (dni_Persona, cargo, sector) VALUES (?, ?, ?)", 48458455, "EXPERIENCIA", "GRUPO");
-            
-            if (User.findFirst("nombreUsuario = ?", "UriCocos4") == null)
-                UserService.createUser("UriCocos4", "UriCocos4", "Uriel", "Luchinni", "1900-05-23", 54118344, "mechylacruz06@gmail.com");
+            }
+            if (User.findFirst("nombreUsuario = ?", "UriCocos4") == null){
+                UserService.createUser("UriCocos4", "UriCocos4", "Uriel", "Luchinni", "1900-05-23", 54118344, "uricocos4@gmail.com");
                 Base.exec("INSERT INTO docente (dni_Persona, legajo, cargo, dni_Administrador) VALUES (?, ?, ?, ?)", 54118344, 452182584, "AYUDANTE", 48458455);
-
-            if (User.findFirst("nombreUsuario = ?", "Leo4") == null)
-                UserService.createUser("Leo4", "Leo4", "Leonel", "Campos", "1900-05-23", 25325125, "mechylacruz06@gmail.com");
+            }
+            if (User.findFirst("nombreUsuario = ?", "Leo4") == null){
+                UserService.createUser("Leo4", "Leo4", "Leonel", "Campos", "1900-05-23", 25325125, "leo4@gmail.com");
                 Base.exec("INSERT INTO estudiante (dni_Persona, estado_Academico, ingreso, dni_Administrador) VALUES (?, ?, ?, ?)", 25325125, "REGULAR", "2020-03-01", 48458455);
-            
+            }
 
             Base.commitTransaction();
 
