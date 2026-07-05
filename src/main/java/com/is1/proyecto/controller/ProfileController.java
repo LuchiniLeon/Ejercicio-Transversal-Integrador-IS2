4bbe0a17278edd703793cd2252a60709ff167c0c
Luchini-Inspiron <luchini.un612@gmail.com>
2026-07-05 18:10:43 -0300

    modificaciones seguridad en profile controller

--- src/main/java/com/is1/proyecto/controller/ProfileController.java
+++ src/main/java/com/is1/proyecto/controller/ProfileController.java
@@ -3,6 +3,9 @@ package com.is1.proyecto.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.is1.proyecto.service.ProfileService;
 
 import spark.ModelAndView;
@@ -11,14 +14,16 @@ import spark.Response;
 
 public class ProfileController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProfileController.class);
+
     public static ModelAndView profile(Request req, Response res) {
 
         Map<String, Object> model = new HashMap<>();
 
         // Verificación de sesión
         if (req.session().attribute("loggedIn") == null) {
+            logger.warn("Intento de acceso a /profile sin iniciar sesión. Redirigiendo al inicio.");
             res.redirect("/");
-            System.out.println("El logueo es nulo");
             return null;
         }
 
@@ -29,11 +34,12 @@ public class ProfileController {
 
         try {
             Map<String, Object> profileData = ProfileService.getProfileData(username);
-            System.out.println("Sigo en el try");
+
             model.putAll(profileData);
     
         } catch (Exception e) {
-            e.printStackTrace(); 
+            logger.error("Error al cargar el perfil", e);
+    
             model.put("errorMessage", "Error al cargar el perfil");
         }
         return new ModelAndView(model, "profile.mustache");
